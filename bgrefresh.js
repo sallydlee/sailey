9301bc19e48197c2fc83f8e87e5876557315b04a
Sally <41641011+sallydlee@users.noreply.github.com>
2020-11-04 22:26:07 -0800

    bgcolor

---
+++ bgrefresh.js
@@ -0,0 +1,3 @@
+var bgColors=new Array("#FFE38D", "#FFCFCF", "#C7FFA1", "#D0C4F7", "#F8B087", "#DABDAA", "#E2E1CF", "#D8D300")
+
+document.body.style.background=bgColors[Math.floor(Math.random()*bgColors.length)]
